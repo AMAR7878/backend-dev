224f32008d1c8d9f86a7b19454c0a7e8d51afa4e
amar <gru6942@gmail.com>
2022-08-19 12:48:46 +0530

    author creation

---
+++ src/controllers/authorController.js
@@ -0,0 +1,24 @@
+const AuthorModel= require("../models/authorModel")
+const BookModel = require("../models/bookModel")
+
+
+ 
+
+const createAuthor= async function (req, res) {
+    let data= req.body
+    let savedData= await AuthorModel.create(data)
+    res.send({msg: savedData})
+}
+
+
+
+const getAuthorsData= async function (req, res) {
+    let savedData = await AuthorModel.find({authorName: "Chetan Bhagat"}).select("authorId")
+    let bookdata = await BookModel.find({authorId: savedData[0].authorId})
+    res.send({msg: bookdata})
+}
+
+
+module.exports.createAuthor= createAuthor
+
+module.exports.getAuthorsData= getAuthorsData
