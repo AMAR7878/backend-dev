85efe19af22b1e05d8643f8b55a2024e91ab8c99
amar <gru6942@gmail.com>
2022-09-01 17:46:28 +0530

    cities weather

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const router = express.Router();
-const CowinController= require("../controllers/cowinController")
+const router = express.Router()
+const Weather = require("../controllers/weatherController")
 
 
 
@@ -8,15 +8,11 @@ const CowinController= require("../controllers/cowinController")
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
-// ------------------------------------------------------- CoWin Data -----------------------------------------------------------------------------
 
-router.get("/cowin/states", CowinController.getStates)
-router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
-router.get("/cowin/getByPin", CowinController.getByPin)
+//----------------------------------------------------------------- Weather Data -------------------------------------------------------------------
 
-// WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
-router.get("/cowin/getDistrictsById", CowinController.getDistrictsById)
-
+router.get("/weather/getWeather", Weather.getWeather)
+router.get("/weather/getCity", Weather.getCity)
 
 
 
